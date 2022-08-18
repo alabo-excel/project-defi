ee900f59b49ab79b9f5a1a8a5f44cf72b9cd1332
Alabo Excel <iamalaboexcel@gmail.com>
2022-08-18 14:21:14 +0100

    img

--- src/views/home.vue
+++ src/views/home.vue
@@ -7,7 +7,7 @@ import Hero from "../components/Hero.vue";
     <Hero />
     <div class=" text-2xl text-center lg:mt-32 mt-52">
       <div class=" w-2/3 mx-auto bold">Fully decentralized and secured globally</div>
-      <img class="lg:w-[70%] mx-auto" src="../assets/global .png" alt="" />
+      <img class="lg:w-[70%] mx-auto" src="../assets/global.png" alt="" />
     </div>
   </div>
 </template>
